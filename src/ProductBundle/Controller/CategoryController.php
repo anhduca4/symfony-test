1affc30ab258a7da9e01e3e64660aadfb58b9abc
Nguyen Anh Duc <ducna@peacesoft.net>
2019-09-03 11:12:37 +0700

    :+1: Init project

---
+++ src/ProductBundle/Controller/CategoryController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ProductBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CategoryController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('category/index.html.twig');
+    }
+}
